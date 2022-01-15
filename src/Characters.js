5d1f9ef4528e53c0f28cdf29153ae02b071031c1
Anshu Patel <anshupatel314@gmail.com>
2022-01-15 15:36:46 -0500

    basic starting point

---
+++ src/Characters.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Characters.css';
+
+class Characters extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>GIANT BOMB API</h1>
+      </div>
+    );
+  }
+}
+
+export default Characters;
