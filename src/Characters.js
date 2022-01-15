8599c17dd55e350650a116744273f4960391377a
Anshu Patel <anshupatel314@gmail.com>
2022-01-15 15:54:05 -0500

    able to call API with secret key

--- src/Characters.js
+++ src/Characters.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import axios from 'axios';
+import secrets from './secrets';
 import './Characters.css';
 
 class Characters extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      characters: [],
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(
+        `https://www.giantbomb.com/api/characters/?api_key=${secrets.API_KEY}&format=json`
+      )
+      .then((res) => {
+        this.setState({ characters: res.data.results });
+        console.log(res.data.results);
+      })
+      .catch((error) => console.log(error));
+  }
+
   render() {
     return (
       <div className="App">
         <h1>GIANT BOMB API</h1>
+        <div className="characters">
+          {this.state.characters.length === 0 && <h1>Loading...</h1>}
+          {this.state.characters.length > 0 &&
+            this.state.characters.map((character) => <h3>{character.name}</h3>)}
+        </div>
       </div>
     );
   }
